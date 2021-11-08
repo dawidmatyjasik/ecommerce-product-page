b4eaf3aaab578d2a3c2d280bcacc2436a78a3c38
Dawid Spisak <dawidspisak@gmail.com>
2021-11-08 22:49:46 +0100

    add amount state

--- src/components/Content/Description/Menu/Menu.js
+++ src/components/Content/Description/Menu/Menu.js
@@ -8,14 +8,22 @@ import {
   MenuSpan,
 } from "../../ContentElements";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
 
 const Menu = () => {
+  const [amount, setAmount] = useState(0);
+
+  const handleSubstract = () => {
+    if (amount > 0) {
+      setAmount(amount - 1);
+    }
+  };
   return (
     <MenuContainer>
       <MenuCounter>
-        <MenuButton>-</MenuButton>
-        <MenuSpan>0</MenuSpan>
-        <MenuButton>+</MenuButton>
+        <MenuButton onClick={handleSubstract}>-</MenuButton>
+        <MenuSpan>{amount}</MenuSpan>
+        <MenuButton onClick={() => setAmount(amount + 1)}>+</MenuButton>
       </MenuCounter>
       <MenuCart>
         <ShoppingCartIcon />
