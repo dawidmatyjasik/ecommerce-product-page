066161a79642858a78127ccccdde05e3e732ed7f
Dawid Spisak <dawidspisak@gmail.com>
2021-11-08 23:13:25 +0100

    improve responsivness

--- src/components/Content/ContentElements.js
+++ src/components/Content/ContentElements.js
@@ -8,7 +8,7 @@ export const ContentContainer = styled.div`
   grid-template-rows: 1fr;
   @media (max-width: 600px) {
     grid-template-columns: 1fr;
-    grid-template-rows: 45vh calc(100vh - 12vh - 45vh);
+    grid-template-rows: 35vh calc(100vh - 12vh - 35vh);
   }
 `;
 
@@ -28,8 +28,11 @@ export const DescriptionContainer = styled.div`
   display: grid;
   justify-content: center;
   align-items: center;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 3fr 3fr 1fr 1fr 1fr;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  /* grid-template-columns: 1fr; */
+  /* grid-template-rows: 5vh 5vh 10vh 5vh 5vh 5vh; */
   padding: calc(5vw) 3vw;
   width: 80%;
   height: 80%;
@@ -50,35 +53,39 @@ export const DescCompany = styled.h2`
   color: ${({ theme }) => theme.colors.primary.orange};
   text-transform: uppercase;
   font-size: 1rem;
+  width: 100%;
   @media (max-width: 768px) {
     font-size: 0.9rem;
   }
   @media (max-width: 600px) {
-    font-size: 0.8rem;
+    font-size: 0.9rem;
   }
 `;
 export const DescTitle = styled.h1`
   font-weight: bold;
   font-size: 3rem;
+  width: 100%;
   @media (max-width: 768px) {
     font-size: 2rem;
   }
   @media (max-width: 600px) {
-    font-size: 1.7rem;
+    font-size: 2rem;
   }
 `;
 export const DescInfo = styled.p`
   color: ${({ theme }) => theme.colors.neutral.darkGrayishBlue};
   font-size: 1rem;
   letter-spacing: 0.4px;
+  width: 100%;
   @media (max-width: 768px) {
     font-size: 0.9rem;
   }
   @media (max-width: 600px) {
-    font-size: 0.8rem;
+    font-size: 1rem;
   }
 `;
 export const DescPrice = styled.h2`
+  width: 100%;
   font-weight: bold;
   font-size: 2rem;
   display: flex;
@@ -87,7 +94,7 @@ export const DescPrice = styled.h2`
     font-size: 1.5rem;
   }
   @media (max-width: 600px) {
-    font-size: 1.3rem;
+    font-size: 1.5rem;
   }
 `;
 
@@ -99,26 +106,27 @@ export const DescPriceDiscount = styled.span`
   padding: 5px 10px;
   border-radius: 5px;
   @media (max-width: 768px) {
-    font-size: 0.75rem;
+    font-size: 0.85rem;
   }
 `;
 
 export const DescSell = styled.h4`
   color: ${({ theme }) => theme.colors.neutral.grayishBlue};
   font-size: 1rem;
+  width: 100%;
   @media (max-width: 768px) {
     font-size: 0.9rem;
   }
   @media (max-width: 600px) {
-    font-size: 1.3rem;
+    font-size: 1.5rem;
   }
 `;
 export const MenuContainer = styled.div`
-  height: 100%;
+  /* height: 100%; */
   width: 100%;
   display: grid;
   grid-template-columns: 2fr 3fr;
-  grid-template-rows: 1fr;
+  grid-template-rows: 10vh;
   @media (max-width: 600px) {
     grid-template-columns: 1fr;
     grid-template-rows: 1fr 1fr;
@@ -127,6 +135,8 @@ export const MenuContainer = styled.div`
   }
 `;
 export const MenuCounter = styled.div`
+  width: 100%;
+  height: 5vh;
   display: grid;
   grid-template-rows: 1fr;
   grid-template-columns: 1fr 1fr 1fr;
@@ -150,6 +160,8 @@ export const MenuSpan = styled.span`
 `;
 
 export const MenuCart = styled.button`
+  height: 5vh;
+  width: 100%;
   outline: none;
   border: none;
   background-color: ${({ theme }) => theme.colors.primary.orange};
